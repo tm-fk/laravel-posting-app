23a3f50ad30d3adba8b8e7aa32541f46c00417fb
tm-fk <mk.1104.rad@outlook.com>
2024-06-09 17:38:26 +0900

    コミット３

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
     // 一覧ページ
     public function index()
     {
-        $posts = Auth::user()->posts()->orderBy('created_at', 'asc')->get();
+        $posts = Auth::user()->posts()->orderBy('updated_at', 'asc')->get();
 
         return view('posts.index', compact('posts'));
     }
